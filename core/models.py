5d4695d7b5241aa75f8e2cf3c4c07d65f6e5185b
henryy07 <adam.harmasz@o2.pl>
2019-01-31 21:21:54 +0100

    added signals to create game record obj and players objects, still not working properly need to be fixed

--- core/models.py
+++ core/models.py
@@ -11,6 +11,11 @@ from django.conf import settings
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+
+from .utils import extract_data_from_game_record_file
+
+User = settings.AUTH_USER_MODEL
 
 
 def profile_image_file_path(instance, filename):
@@ -32,7 +37,7 @@ def gomoku_record_image_file_path(instance, filename):
 class Profile(models.Model):
     """Class defining User object linked to profile by one-to-one"""
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE
     )
     picture = models.ImageField(null=True, upload_to=profile_image_file_path)
@@ -50,8 +55,8 @@ class Profile(models.Model):
 class Player(models.Model):
     """Class defining player object"""
     nickname = models.CharField(max_length=255)
-    win = models.IntegerField()
-    loss = models.IntegerField()
+    win = models.IntegerField(blank=True, null=True)
+    loss = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.nickname
@@ -59,7 +64,7 @@ class Player(models.Model):
 
 class GomokuRecord(models.Model):
     """Class defining gomoku record object"""
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
     game_record = models.TextField()
     game_date = models.DateTimeField()
     black_player = models.ForeignKey(
@@ -83,19 +88,41 @@ class GomokuRecord(models.Model):
 
 class GomokuRecordFile(models.Model):
     """Class defining GomokuRecordFiles objects"""
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
     game_record_file = models.FileField(
         null=True,
         upload_to=gomoku_record_image_file_path
     )
     # file_path = models.CharField(max_length=255, default=None, null=True)
 
+    def __str__(self):
+        return f'game record owned by: {self.profile}'
 
-# @receiver(post_save, sender=GomokuRecordFile)
-# def create_gomoku_record_object_profile(sender, instance, created, **kwargs):
-#     if created:
-#         # Parent.objects.create(name_of_parent=instance)
-#         pass
-#
+
+@receiver(post_save, sender=GomokuRecordFile)
+def create_gomoku_record_object_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create players and game record object after file is uploaded
+    """
+    if created:
+        payload = extract_data_from_game_record_file(
+            f'config/media/{instance.game_record_file}'
+        )
+        Player.objects.create(nickname=instance)
+        white_player = Player.objects.get_or_create(nickname=payload['white'])
+        black_player = Player.objects.get_or_create(nickname=payload['black'])
+        print(white_player, black_player)
+        # GomokuRecord.objects.create(
+        #     profile=get_user_model().objects.get(username=instance.profile),
+        #     game_record=payload['game_record'],
+        #     game_date=payload['date_time'],
+        #     result=payload['result'],
+        #     swap=payload['swap'],
+        #     swap_2=payload['swap2'],
+        #     color_change=payload['color_change'],
+        #     black_player=black_player,
+        #     white_player=white_player,
+        # )
 #
 # @receiver(post_save, sender=GomokuRecordFile)
 # def save_user_profile(sender, instance, **kwargs):
--- gomoku_app/views.py
+++ gomoku_app/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from core import models
 from . import forms
 from core.utils import extract_data_from_game_record_file
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class ExtractDataRedirectView(View):
+class ExtractDataRedirectView(LoginRequiredMixin, View):
     """
     View which will unpack data from gomoku
     record file and pass it to serializer
@@ -19,11 +23,15 @@ class ExtractDataRedirectView(View):
 
     def post(self, request):
         form = forms.GomokuRecordForm(request.POST, request.FILES)
+        user = User.objects.get(username=self.request.user)
+        print('dupa', user)
         if form.is_valid():
             files = form.cleaned_data['file']
-            gomoku_file = models.GomokuRecordFile(game_record_file=files)
-            gomoku_file.save()
-            payload = extract_data_from_game_record_file(f'config/media/{gomoku_file.game_record_file}')
-        return HttpResponse(payload['game_record'])
+            gomoku_file = models.GomokuRecordFile.objects.create(
+                game_record_file=files,
+                profile=user
+            )
+            # gomoku_file.save()
+        return HttpResponse('dupa')
 
 
