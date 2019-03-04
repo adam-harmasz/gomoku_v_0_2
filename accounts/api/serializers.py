7723cdb32a6cf36b88e0479d200b26524a4a0281
henryy07 <adam.harmasz@o2.pl>
2019-03-04 19:27:05 +0100

    added ajax to edit user form, form is already filled with actual data

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'first_name', 'password')
         # extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
