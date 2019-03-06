f1baea1a32cda24d577fd92454b088693088d008
henryy07 <adam.harmasz@o2.pl>
2019-03-06 11:54:24 +0100

    password change moved to config urls due to namespace accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,12 +15,7 @@ urlpatterns = [
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(
              template_name='registration/logout.html'), name='logout'),
-    # Password change
-    path('password_change/', auth_views.PasswordChangeView.as_view(),
-         name='password_change'),
-    path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
+
     # password reset views
     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name='password_reset'),
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from .routers import router
 
@@ -27,5 +28,10 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('api/', include(router.urls)),
     path('gomoku/', include('gomoku_app.urls', namespace='gomoku')),
+    # Password change
+    path('password_change/', auth_views.PasswordChangeView.as_view (),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
