42f32e031acf0f1d10c90b84daadacad657c64b0
henryy07 <adam.harmasz@o2.pl>
2019-03-06 13:21:26 +0100

    updating profile pic with ajax

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,19 +15,7 @@ urlpatterns = [
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(
              template_name='registration/logout.html'), name='logout'),
-
-    # password reset views
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('reset/done', auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
     # detail view
     path('profile/<slug:slug>/', views.UserProfileView.as_view(),
          name='profile-detail'),
-
 ]
